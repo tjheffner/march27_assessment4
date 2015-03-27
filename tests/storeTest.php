56be335872513613f6947c2cf654ca4485916e16
Tanner Heffner <tannerjheffner@gmail.com>
2015-03-27 09:06:54 -0700

    Add tests for getters & setters

--- tests/storeTest.php
+++ tests/storeTest.php
@@ -34,46 +34,81 @@
     function test_getAddress()
     {
       //Arrange
+      $name = "More Shoes";
+      $address = "234 Runner's Way";
+      $id = 1;
+      $test_store = new Store($name, $address, $id);
 
       //Act
+      $result = $test_store->getAddress();
 
       //Assert
+      $this->assertEquals($address, $result);
     }
 
     function test_getId()
     {
-      //Arrange
+      $name = "Shoes on Shoes on Shoes";
+      $address = "345 Couch St.";
+      $id = 1;
+      $test_store = new Store($name, $address, $id);
 
       //Act
+      $result = $test_store->getId();
 
       //Assert
+      $this->assertEquals(1, $result);
     }
 
     function test_setName()
     {
       //Arrange
+      $name = "Kicks R Us";
+      $address = "456 Mall";
+      $id = 1;
+      $test_store = new Store($name, $address, $id);
+      $new_name = "Kicks For All";
 
       //Act
+      $test_store->setName($new_name);
 
       //Assert
+      $result = $test_store->getName();
+      $this->assertEquals($new_name, $result);
     }
 
     function test_setAddress()
     {
       //Arrange
+      $name = "Kicks R Us";
+      $address = "456 Mall";
+      $id = 1;
+      $test_store = new Store($name, $address, $id);
+      $new_address = "567 Next Door";
 
       //Act
+      $test_store->setAddress($new_address);
 
       //Assert
+      $result = $test_store->getAddress();
+      $this->assertEquals($new_address, $result);
     }
 
     function test_setId()
     {
       //Arrange
+      $name = "Kicks R Us";
+      $address = "456 Mall";
+      $id = 1;
+      $test_store = new Store($name, $address, $id);
+      $new_id = 2;
 
       //Act
+      $test_store->setId(2);
 
       //Assert
+      $result = $test_store->getId();
+      $this->assertEquals(2, $result);
     }
 
 
